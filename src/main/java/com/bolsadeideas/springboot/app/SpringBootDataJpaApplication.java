8f87d50d568767d64df62aaee6bcc2b7a4a06f58
jjas420 <ayosuajon4@gmail.com>
2024-05-19 18:17:30 -0400

    terminada la seccion de segurity

--- src/main/java/com/bolsadeideas/springboot/app/SpringBootDataJpaApplication.java
+++ src/main/java/com/bolsadeideas/springboot/app/SpringBootDataJpaApplication.java
@@ -6,22 +6,34 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.bolsadeideas.springboot.app.models.service.IUploadFileService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class SpringBootDataJpaApplication  implements CommandLineRunner {
+public class SpringBootDataJpaApplication implements CommandLineRunner {
 
-	@Autowired
-	IUploadFileService uploadFileService;
-	
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootDataJpaApplication.class, args);
-	}
+    @Autowired
+    IUploadFileService uploadFileService;
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
 
-	@Override
-	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
-		uploadFileService.deleteAll();
-		uploadFileService.init();
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDataJpaApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        // TODO Auto-generated method stub
+        uploadFileService.deleteAll();
+        uploadFileService.init();
+        String password = "12345";
+        
+        for (int i=0;i<2;i++){
+            String bcryptPassword= passwordEncoder.encode(password);
+            System.out.println(bcryptPassword);
+            
+            
+        }
+
+    }
 
 }
--- src/main/java/com/bolsadeideas/springboot/app/SpringSegurityConfig.java
+++ src/main/java/com/bolsadeideas/springboot/app/SpringSegurityConfig.java
@@ -5,75 +5,76 @@
 package com.bolsadeideas.springboot.app;
 
 import com.bolsadeideas.springboot.app.auth.handler.LoginSuccesHandler;
+import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-    import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-    import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-    import org.springframework.security.core.userdetails.User;
-    import org.springframework.security.core.userdetails.UserDetailsService;
-    import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-    import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-    import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
 /**
  *
  * @author ayosu
  */
 @Configuration
 @EnableMethodSecurity(securedEnabled = true, prePostEnabled = true)
-public class SpringSegurityConfig{
-    @Autowired
-	private LoginSuccesHandler successHandler;
+public class SpringSegurityConfig {
 
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    @Autowired
+    private LoginSuccesHandler successHandler;
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+    @Autowired
+    private DataSource dataSource;
+
+    @Autowired
+    public void configurerGlobal(AuthenticationManagerBuilder build) throws Exception {
+        
+        build.jdbcAuthentication().dataSource(dataSource).passwordEncoder(passwordEncoder).usersByUsernameQuery(
+        "select username,password,enabled from users where username=?").authoritiesByUsernameQuery(
+        "select u.username, a.authority from authorities a inner join users u on (a.user_id=u.id) where u.username=?");
+
+        /*
+		PasswordEncoder encoder =this.passwordEncoder;
+		UserBuilder users = User.builder().passwordEncoder(encoder::encode);
+		
+		build.inMemoryAuthentication()
+		.withUser(users.username("admin").password("12345").roles("ADMIN", "USER"))
+		.withUser(users.username("jonathan").password("12345").roles("USER"));
+         */
     }
 
-         @Bean
-        public UserDetailsService userDetailsService()throws Exception{
-                    
-            InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-            manager.createUser(User
-                    .withUsername("user")
-                    .password(passwordEncoder().encode("user"))
-                    .roles("USER")
-                    .build());
-             manager.createUser(User
-                        .withUsername("admin")
-                        .password(passwordEncoder().encode("admin"))
-                        .roles("ADMIN","USER")
-                        .build());
-            
-            return manager;
-            
-            
-        }
-         @Bean
-        public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-     
-          http.authorizeRequests().requestMatchers("/","/index", "/csss/**", "/js/**", "/images/**", "/listar").permitAll()
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests().requestMatchers("/", "/index", "/csss/**", "/js/**", "/images/**", "/listar").permitAll()
                 //  .requestMatchers("/ver/**").hasAnyRole("USER")
                 //    .requestMatchers("/uploads/**").hasAnyRole("USER")
                 //    .requestMatchers("/form/**").hasAnyRole("ADMIN")
-                 //   .requestMatchers("/eliminar/**").hasAnyRole("ADMIN")
-                  //  .requestMatchers("/factura/**").hasAnyRole("ADMIN")
-                  .anyRequest().authenticated()
-		.and()
-		    .formLogin()
-		        .successHandler(successHandler)
-		        .loginPage("/login")
-		    .permitAll()
-		.and()
-		.logout().permitAll()
-		.and()
-		.exceptionHandling().accessDeniedPage("/error_403");
+                //   .requestMatchers("/eliminar/**").hasAnyRole("ADMIN")
+                //  .requestMatchers("/factura/**").hasAnyRole("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .successHandler(successHandler)
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout().permitAll()
+                .and()
+                .exceptionHandling().accessDeniedPage("/error_403");
 
-             
-                
-           
         return http.build();
-        }
+    }
 
 }
